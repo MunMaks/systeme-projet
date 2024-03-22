84be903578bed1fd43ac9a0d0454ea479fd94451
Maks <maksmunait@gmail.com>
2024-03-22 17:14:04 +0100

    before_csv

---
+++ src/main.py
@@ -0,0 +1,13 @@
+"""
+22 mars 2024, Copyright MUNAIT Maks
+Université Gustave Eiffel
+"""
+import subprocess
+
+def main():
+    print("hello world")
+    pass
+
+
+if __name__ == '__main__':
+    main()
