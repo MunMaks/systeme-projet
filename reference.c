73c30cb83faea76bf632a4f6a194323889135df1
Maks <98954111+MunMaks@users.noreply.github.com>
2024-03-25 17:15:52 +0100

    Update reference.c

--- reference.c
+++ reference.c
@@ -1,8 +1,12 @@
+/* import les bibliotheques necessaires */
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char *argv[]){
-    if (argc < 3) return 0;
-    printf("%d\n", atoi(argv[1]) + atoi(argv[2]));
-    return 0;
+
+    if (argc != 3) return 0;    /* si nous avons assez d'arguments */
+
+    printf("%d\n", atoi(argv[1]) + atoi(argv[2]));  /* afficher le resultat */
+
+    return 0;   /* sortie de la programme */
 }
